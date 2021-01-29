603a6b115e9d13ed998b661e2ae2478939628138
Fabio <Morini>
2021-01-29 18:24:34 +0100

    Prototype

---
+++ Assets/Scripts/HearthController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HearthController : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D rb;
+	private Vector2 force;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		force = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed).normalized;
+		rb.MovePosition(rb.position + force * speed * Time.fixedDeltaTime);
+	}
+}
