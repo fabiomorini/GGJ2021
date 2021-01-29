1d032a6cf64d63c036befa1a95c68ff7eebdec2c
Antonio <antonio.robreno@enti.cat>
2021-01-29 19:19:14 +0100

    blood

---
+++ Assets/Scripts/Blood.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+    public GameObject Player;
+    bool isTouching = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTouching = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isTouching)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player.GetComponent<HearthController>().SetBlood();
+            }
+        }
+    }
+}
--- Assets/Scripts/HearthController.cs
+++ Assets/Scripts/HearthController.cs
@@ -7,15 +7,41 @@ public class HearthController : MonoBehaviour
 	public float speed;
 	private Rigidbody2D rb;
 	private Vector2 force;
+	public float blood;
+	[HideInInspector]
+	public float maxBlood = 15f;
+	private float timer = 0.0f;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		SetBlood();
 	}
 
 	private void FixedUpdate()
 	{
 		force = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed).normalized;
 		rb.MovePosition(rb.position + force * speed * Time.fixedDeltaTime);
+	}
+
+    private void Update()
+    {
+		DrainBlood();
+    }
+
+	private void DrainBlood()
+    {
+		if (blood > 0)
+		{
+			timer -= Time.deltaTime;
+			blood = timer % 60;
+			Debug.Log((int)blood);
+		}
+	}
+
+	public void SetBlood()
+    {
+		blood = maxBlood + 1;
+		timer = maxBlood;
 	}
 }
