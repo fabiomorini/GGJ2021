b02d4ed727f4c11022bb705414c0061c0f22d9f9
Fabio <Morini>
2021-01-30 23:39:32 +0100

    fix

---
+++ Assets/Scripts/CharacterWaitingRoom.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DragonBones;
+
+public class CharacterWaitingRoom : MonoBehaviour
+{
+	private float speed = 15.0f;
+	private Rigidbody2D rb;
+	float horizontal;
+	float vertical;
+	float moveLimiter = 0.7f;
+
+	private float blood;
+	private float maxBlood;
+	public Slider slider;
+
+	[HideInInspector] public UnityArmatureComponent anim;
+
+	private void Start()
+	{
+		maxBlood = 10.0f;
+		anim = GetComponentInChildren<UnityArmatureComponent>();
+		rb = GetComponent<Rigidbody2D>();
+		SetBlood();
+		slider.maxValue = maxBlood;
+		slider.value = blood;
+		anim.animation.Play(("Idle_Walk"), -1);
+	}
+
+	private void FixedUpdate()
+	{
+		if (horizontal != 0 && vertical != 0)
+		{
+			horizontal *= moveLimiter;
+			vertical *= moveLimiter;
+		}
+
+		rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+	}
+
+	private void Update()
+	{
+		//movement
+		horizontal = Input.GetAxisRaw("Horizontal");
+		vertical = Input.GetAxisRaw("Vertical");
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			speed = 25.0f;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			speed = 15.0f;
+		}
+	}
+
+	public void SetBlood()
+	{
+		blood = maxBlood + 1;
+	}
+}
