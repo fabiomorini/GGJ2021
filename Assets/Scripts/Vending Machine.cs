5a193b1a5a7ecb5db2094ae833d651fbfebf0188
Fabio <Morini>
2021-01-30 21:55:47 +0100

    elevator

---
+++ Assets/Scripts/Vending Machine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendingMachine : MonoBehaviour
+{
+    public GameObject elevatorUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            elevatorUI.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            elevatorUI.SetActive(false);
+        }
+    }
+}
