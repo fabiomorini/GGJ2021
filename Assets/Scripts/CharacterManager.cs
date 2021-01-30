0fcc6ee189fd443e4ae5a97c45d8aef0d8f75257
Fabio <Morini>
2021-01-30 23:27:45 +0100

    characterManager

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    [HideInInspector]public int coins;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
